575400aa36c2b7d6ed640f59ad52e39dc8d6c1a6
shifu <shifu.wu@outlook.com>
2023-03-05 15:40:44 +0800

    try to enable static build

--- ffmpeg-android-maker.sh
+++ ffmpeg-android-maker.sh
@@ -34,7 +34,7 @@ export BUILD_DIR_EXTERNAL=$BUILD_DIR/external
 function prepareOutput() {
   OUTPUT_LIB=${OUTPUT_DIR}/lib/${ANDROID_ABI}
   mkdir -p ${OUTPUT_LIB}
-  cp ${BUILD_DIR_FFMPEG}/${ANDROID_ABI}/lib/*.so ${OUTPUT_LIB}
+  cp ${BUILD_DIR_FFMPEG}/${ANDROID_ABI}/lib/* ${OUTPUT_LIB}
 
   OUTPUT_HEADERS=${OUTPUT_DIR}/include/${ANDROID_ABI}
   mkdir -p ${OUTPUT_HEADERS}
@@ -118,7 +118,7 @@ do
     cd ${BASE_DIR}
   done
 
-  checkTextRelocations || exit 1
+  # checkTextRelocations || exit 1
 
   prepareOutput
 done
